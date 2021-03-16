b42cea05a36decefc5c919f0d86ce5e2d6c09daf
Samson SSali <jitpomi@gmail.com>
2021-03-16 16:14:15 -0700

    subscriptions with apollo-server inbuilt pub/sub

--- nexus/context.ts
+++ nexus/context.ts
@@ -1,33 +1,35 @@
 require('dotenv').config()
 
-import { PubSub } from 'apollo-server'
+// import { PubSub } from 'apollo-server'
 import { PrismaClient } from '@prisma/client'
-// import { PostgresPubSub } from "graphql-postgres-subscriptions";
-// import { Client } from "pg";
+import { PostgresPubSub } from "graphql-postgres-subscriptions";
+import { Client } from "pg";
 
 
 
 
 export interface Context{
   prisma: PrismaClient
- //  pubsub: PostgresPubSub 
- pubsub: PubSub
+  pubsub: PostgresPubSub 
+// pubsub: PubSub
 }
 
 
 const prisma = new PrismaClient()
-const pubsub = new PubSub()
+// const pubsub = new PubSub()
 
-// const connectionString = process.env.DATABASE_URL;
+ const connectionString = process.env.DATABASE_URL;
 
-// const client = new Client({connectionString});
+ const client = new Client({
+  connectionString: connectionString
+});
+client.connect();
 
-// const pubsub = new PostgresPubSub({ client });
+const pubsub = new PostgresPubSub({ client }); // const pubsub = new PubSub()
 
 
 
-
-export const context: Context = {
+export const context: Context= {
   prisma,
   pubsub,
 }
